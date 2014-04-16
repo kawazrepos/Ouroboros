2a101728c98abb69cb4091b719fec314e9e08db9
giginet <giginet.net@gmail.com>
2014-04-17 07:14:55 +0900

    Add blogs schema

--- converter.py
+++ converter.py
@@ -1,25 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime, Date
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class Entry(Base):
-    __tablename__ = 'blogs_entry'
-
-    id = Column(Integer, primary_key=True)
-    pub_state = Column(String)
-    title = Column(String)
-    body = Column(String)
-    body_markup_type = Column(String)
-    category_id = Column(Integer)
-    _body_rendered = Column(String)
-    author_id = Column(Integer)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
-    publish_at = Column(DateTime)
-    publish_at_date = Column(Date)
+from ouroboros.schemas.src.blogs import Entry
 
 if __name__ == '__main__':
 
